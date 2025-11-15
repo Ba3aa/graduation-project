bfcece059f28d946109bd3797287d13a76e1a89f
Ba3aa <baharaed9@gmail.com>
2025-11-16 01:28:41 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import LoginPage from './pages/loginpage';
+
+function App() {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
