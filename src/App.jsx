3bcd14eecf1157bf8d3bea081b8016c8b0f6717a
Ba3aa <baharaed9@gmail.com>
2025-11-16 01:35:43 +0300

    login-mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import LoginPage from './pages/loginpage';
+import React, { useState, useEffect } from "react";
+import LoginPage from "./pages/loginpage";
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
     document.body.className = theme;
   }, [theme]);
 
   const toggleTheme = () => {
-    setTheme(theme === 'light' ? 'dark' : 'light');
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
   return (
     <div className="App">
-     
+      <LoginPage onToggleTheme={toggleTheme} />
     </div>
   );
 }
