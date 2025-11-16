043f6233c31fa43834d47c03630ed4c091f02559
ahmad-abdelraheem <ahmadabdelraheem@outlook.com>
2025-11-17 01:02:08 +0300

    fix login page usage in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div className="App">
-      <login-page currentTheme={theme} onToggleTheme={toggleTheme} />
+      <LoginPage currentTheme={theme} onToggleTheme={toggleTheme} />
     </div>
   );
 }
